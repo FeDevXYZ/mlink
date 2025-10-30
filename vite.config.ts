92dba0da0825481542e4f0fa20ae7e4869cbf3fd
unknown <fe.dev.xyz@gmail.com>
2025-10-30 18:55:52 +0100

    aiuto

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/mlink/'  
+    base: '/mlink/', 
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
